056d39535c68137472eb260624674d9655cd4053
rajith.p.aet@gmail.com <rajith.p.aet@gmail.com>
2022-04-25 00:07:35 +0530

    SearchUserRepository implementation completed

--- app/src/main/java/com/rajith/payconiq/home/search/data/repository/SearchUserRepositoryImpl.kt
+++ app/src/main/java/com/rajith/payconiq/home/search/data/repository/SearchUserRepositoryImpl.kt
@@ -5,13 +5,33 @@ import com.rajith.payconiq.common.util.Resource
 import com.rajith.payconiq.home.search.data.remote.SearchUserService
 import com.rajith.payconiq.home.search.domain.repository.SearchUserRepository
 import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.flow
+import retrofit2.HttpException
+import java.io.IOException
 
 class SearchUserRepositoryImpl (
     private val api: SearchUserService
 ) : SearchUserRepository {
 
-    override fun searchUser(query: String, pageNumber: Int): Flow<Resource<UserResponse>> {
-        TODO("Not yet implemented")
+    override fun searchUser(searchQuery: String, pageNumber: Int): Flow<Resource<UserResponse>> = flow {
+        emit(Resource.Loading())
+        val users: UserResponse
+        try {
+            users = api.searchUser(query = searchQuery, pageNumber = pageNumber)
+            emit(Resource.Success(users))
+        } catch (e: HttpException) {
+            emit(
+                Resource.Error(
+                    message = "Oops, something went wrong!"
+                )
+            )
+        } catch (e: IOException) {
+            emit(
+                Resource.Error(
+                    message = "Couldn't reach server, check your internet connection."
+                )
+            )
+        }
     }
 
 
