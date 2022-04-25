9d40783dac5ecf59139a0457f3a5a7be159edc30
rajith.p.aet@gmail.com <rajith.p.aet@gmail.com>
2022-04-26 00:51:56 +0530

    UserDetail Use case added

--- app/src/main/java/com/rajith/payconiq/home/detail/di/UserDetailModule.kt
+++ app/src/main/java/com/rajith/payconiq/home/detail/di/UserDetailModule.kt
@@ -2,6 +2,7 @@ package com.rajith.payconiq.home.detail.di
 
 import com.rajith.payconiq.home.detail.data.repository.UserDetailRepositoryImpl
 import com.rajith.payconiq.home.detail.domain.repository.UserDetailRepository
+import com.rajith.payconiq.home.detail.domain.usecase.UserDetailUseCase
 import dagger.Module
 import dagger.Provides
 import dagger.hilt.InstallIn
@@ -14,5 +15,10 @@ class UserDetailModule {
     @Provides
     fun provideUserDetailRepository(userDetailRepositoryImpl: UserDetailRepositoryImpl): UserDetailRepository {
         return userDetailRepositoryImpl
+    }
+
+    @Provides
+    fun provideUserDetailUseCase(userDetailRepository: UserDetailRepository): UserDetailUseCase {
+        return UserDetailUseCase(userDetailRepository)
     }
 }
